e8defade224135c37de097b2a41bdeec843c5a30
MilesChou <jangconan@gmail.com>
2016-10-12 23:02:15 +0800

    Add migrations

---
+++ database/migrations/20161012145531_initialize.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Initialize extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('confs', ['id' => 'id']);
+        $table->addColumn('slug', 'string')
+              ->addColumn('title', 'string')
+              ->addColumn('subject', 'string')
+              ->addColumn('date', 'datetime')
+              ->addColumn('location', 'string')
+              ->addColumn('url', 'string')
+              ->addColumn('hash', 'string')
+              ->create();
+    }
+}
