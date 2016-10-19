17bb74f5e452b642ef2f51ca01fbc31528246e6f
MilesChou <jangconan@gmail.com>
2016-10-19 10:43:34 +0800

    Add Eloquent

--- app.php
+++ app.php
@@ -1,11 +1,38 @@
 <?php
 declare(strict_types=1);
 
+use Illuminate\Database\Capsule\Manager as Capsule;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-$container = new Slim\Container();
+$setting = [
+    'settings' => [
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'database' => 'default',
+            'username' => 'root',
+            'password' => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+    ]
+];
+
+$container = new Slim\Container($setting);
 $container['settings']['displayErrorDetails'] = true;
+$container['db'] = function ($container) {
+    $capsule = new Capsule();
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
 
 $app = new Slim\App($container);
 
